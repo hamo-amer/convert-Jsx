21bc62647da3524a216aa7fb6dde66a8e0061cf4
Mohamed <mohamedtaher.benarbia@gmail.com>
2020-07-20 00:02:26 +0200

    convert-jsx

---
+++ convertjsx/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import imageInSrc from "./imageInSrc.jpg";
+import './Style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>  
+      <h1 className={'title red'}> Your name here </h1>  
+     <div> <img src={imageInSrc} alt="photosrc"/> </div>
+      <img src="/imageInPublic.jpg" alt="photopublic" />  
+      </div>
+      <video width="320" height="240" controls>  
+ 
+ <source src="/myVideo.mp4" type="video/mp4" /> 
+
+</video> 
+    </div>
+  );
+}
+
+export default App;
